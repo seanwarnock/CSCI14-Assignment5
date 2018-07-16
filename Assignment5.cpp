34beaa32e171bdf51ca5422c276a77de45cc9a98
Sean Warnock <swarnock@warnocksolutions.com>
2018-07-15 20:44:21 -0700

    Initial commit

---
+++ Assignment5.cpp
@@ -0,0 +1,56 @@
+/*
+Sean Warnock
+CSCI 14 Summer 2018
+Assignment 5 due 7/19/2018
+https://github.com/seanwarnock/CSCI14-Assignment5.git
+
+
+Employee Full name
+Employee ID
+Hours Worked (maximum 60 hours)
+Pay Rate for this employee
+Federal Tax Rate (maximum 0. or 50%)
+State Tax Rate (maximum 0.3 or 30%)
+
+In this assignment, we will use a function to compute:
+Regular Pay (40 hours or less)
+Overtime Pay if any (over 40 hours)
+Gross Pay (Regular Pay plus Overtime if any)
+Federal Taxes Witheld
+State WTaxes Withheld
+Social Security Taxes Witheld (6.2% of Gross)
+Medicare Taxes Withheld (1.45% of Gross)
+Net Pay
+*/
+
+#include <time.h>
+#include <iostream>
+#include <iomanip>
+#include <math.h>
+#include <fstream>
+#ifdef _WIN32
+  #include <winsock2.h>
+  #include <windows.h>
+#endif
+#ifdef __linux__
+
+#endif
+
+float PayCalculater (float, float, float, float, float, int);
+
+int main()
+{
+
+
+
+
+}
+
+float PayCalculater (float, float, float, float, float, int)
+{
+/*
+This function will calculate one of the requested pay functions and return
+it's value
+*/
+
+}
